e898e6e27b792d8896493995a917d67050cee6ed
Tenshi <tenshii.tv@gmail.com>
2022-06-10 17:02:54 +0200

    fix guild commands

--- helpers/deployGuildCmd.js
+++ helpers/deployGuildCmd.js
@@ -17,10 +17,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 (async () => {
     try {
         console.log('Deployment of guild commands...');
-        await rest.put(Routes.applicationGuildCommands(process.env.CLIENTID,
-            process.env.GUILDID),
-            { body: commands }
-        );
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), { body: commands });
         console.log('Successfully deployed guild commands.');
     } catch (error) {
         console.error(error);
